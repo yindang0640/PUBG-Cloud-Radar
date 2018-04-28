9225b9e8365d69e0b86efd2f4c7f6d7f0bfdf14e
234150476 <234150476@qq.com>
2018-04-28 22:36:10 +0800

    Add files via upload

--- update.sh
+++ update.sh
@@ -28,7 +28,7 @@ cd libpcap-1.8.1
 make
 make install
 
-git clone https://github.com/794959818/PUBG-Radar-Onekey.git
+git clone https://github.com/234150476/PUBG-Radar-Onekey.git
 cd PUBG-Radar-Onekey/
 npm i
 npm i -g pino
